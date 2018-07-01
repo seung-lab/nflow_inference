5d1dc5e648a0e3fe51965c755bbc381f3dfc5cd4
Sergiy Popovych <sergiy.popovich@gmail.com>
2018-06-30 21:41:33 -0400

    new dockers

--- run_worker_docker_cpu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it seunglab/nflow_inference:cpu bash
--- run_worker_docker_gpu.sh
+++ run_worker_docker_gpu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it seunglab/nflow_inference:gpu bash
+nvidia-docker run -v ~/.cloudvolume/secrets:/root/.cloudvolume/secrets -it seunglab/nflow_inference:gpu bash
